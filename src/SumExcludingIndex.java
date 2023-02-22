17ab7db4135a3f092b8ceccadab675dc2d06c576
Gabriel Valiente <gabriel.valiente917@gmail.com>
2023-02-22 18:18:52 +0800

    first commit

---
+++ src/SumExcludingIndex.java
@@ -0,0 +1,20 @@
+public class SumExcludingIndex {
+
+    public static void main(String[] args){
+
+    }
+    //Here's another sample problem from TestGorilla:
+    //
+    //Problem: Write a Java program that takes an array of integers and returns a
+    // new array where each element is the sum of all the elements in the input array
+    // except for the element at the current index.
+
+    //For example, if the input array is [1, 2, 3, 4], the output array would be [9, 8, 7, 6].
+    //Can you write a Java program to solve this problem?
+
+    public static int[] sum(int[] x){
+        return null;
+    }
+
+
+}
