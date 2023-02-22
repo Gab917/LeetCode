3324e676e4942bf18af4a588581b1eaaee0806b5
Gabriel Valiente <gabriel.valiente917@gmail.com>
2023-02-22 21:33:43 +0800

    Added explanation to RomanToInteger

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -2,22 +2,22 @@ class RomanToInteger {
     public int romanToInt(String s) {
         int result = 0;
         int prev = 0;
-        for (int i = s.length()-1;i>=0;i--){
-            int curr = romanToDecimal(s.charAt(i));
+        for (int i = s.length()-1;i>=0;i--){// Loop iterates from last index to first index (index 0)
+            int curr = romanToDecimal(s.charAt(i));//Converts Roman numeral to Integer value
 
-            if(curr >= prev){
+            if(curr >= prev){ //If current index value is greater than previous, adds current index to the result
                 result += curr ;
             }
-            else if (curr < prev){
+            else if (curr < prev){ //If current index value is less than previous, subtracts current index to the result
                 result -= curr;
             }
-            prev = curr;
+            prev = curr; //Stores current value to prev value
         }
         return result;
 
     }
 
-    public int romanToDecimal(char c){ //
+    public int romanToDecimal(char c){ //Helper method to convert character value of Roman numeral to Integer
         switch (c){
             case 'I':
                 return 1;
